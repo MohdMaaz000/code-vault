a33414b65c5e080c7ceb1ab5fe497e1a393a1334
Mohd Maaz <Mohd.maaz.040323@gmail.com>
2026-07-20 16:51:51 +0530

    [LeetCode][Medium] 0008-string-to-integer-atoi - Time: 3 ms (36.23%), Space: 19.4 MB (16.07%) - AlgorithmHub

---
+++ Leetcode/Medium/0008. String to Integer (atoi)/0008-string-to-integer-atoi.py
@@ -0,0 +1,31 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+        if not s:
+            return 0
+        
+        sign = 1
+        i = 0
+        
+        if s[0] == '-':
+            sign = -1
+            i += 1
+        elif s[0] == '+':
+            i += 1
+            
+        res = 0
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+        
+        while i < len(s) and s[i].isdigit():
+            res = res * 10 + int(s[i])
+            i += 1
+            
+        res *= sign
+        
+        if res < INT_MIN:
+            return INT_MIN
+        if res > INT_MAX:
+            return INT_MAX
+            
+        return res 
