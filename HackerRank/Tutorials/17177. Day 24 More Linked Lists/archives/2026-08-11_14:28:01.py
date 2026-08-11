062bf61407884698a881837393da9810e530a7a5
Mohd Maaz <Mohd.maaz.040323@gmail.com>
2026-08-11 10:57:47 +0530

    [HackerRank][Tutorials][30 Days of Code] Day 24: More Linked Lists - Score: 30.0 - AlgorithmHub

---
+++ HackerRank/Tutorials/17177. Day 24 More Linked Lists/archives/2026-08-11_14:28:01.py
@@ -0,0 +1,12 @@
+
+
+    def removeDuplicates(self,head):
+        current = head
+
+        while current and current.next:
+            if current.data == current.next.data:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
